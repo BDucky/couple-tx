a037e71ef7fbf21b0df6df997a36b172afcaf5c7
Jackietruong2805 <truonghao2805@gmail.com>
2023-06-17 22:28:02 +0700

    Mix&Match no image render

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      {/* MIX & MATCH */}
+      <div className="container py-[45px] px-[30px] text-left bg-grayE">
+        <h1 className="mb-4 text-4xl font-bold">MIX & MATCH</h1>
+        <p className="mb-3 text-gray-500">
+          Thoả sức sáng tạo với vô số sự kết hợp từ món đồ bạn thích.
+        </p>
+        <button className="py-[13px] px-[12px] bg-black text-white text-[12px] font-bold">
+          SHOP NOW
+        </button>
+        <div className="grid grid-cols-4">Image</div>
+      </div>
+    </div>
+  );
 }
