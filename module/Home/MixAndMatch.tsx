4e23cb52b562ac12d06c32afa78ff4f47c10a7e1
Jackietruong2805 <truonghao2805@gmail.com>
2023-06-18 11:01:56 +0700

    edit padding for mix and Match

--- module/Home/MixAndMatch.tsx
+++ module/Home/MixAndMatch.tsx
@@ -2,7 +2,7 @@ import HomeButton from "./HomeButton";
 
 const MixAndMatch = () => {
   return (
-    <div className="container py-[45px] px-[30px] text-left bg-grayE">
+    <div className="container py-[90px] px-[30px] text-left bg-grayE">
       <h1 className="mb-4 text-4xl font-bold">MIX & MATCH</h1>
       <p className="mb-3 text-gray-500">
         Thoả sức sáng tạo với vô số sự kết hợp từ món đồ bạn thích.
