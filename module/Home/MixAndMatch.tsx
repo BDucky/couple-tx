a4006c299bd759d06b26e7624e9348a41bbf309d
Jackietruong2805 <truonghao2805@gmail.com>
2023-06-18 10:57:31 +0700

    Optimize Button for MixAndMatch

--- module/Home/MixAndMatch.tsx
+++ module/Home/MixAndMatch.tsx
@@ -1,3 +1,5 @@
+import HomeButton from "./HomeButton";
+
 const MixAndMatch = () => {
   return (
     <div className="container py-[45px] px-[30px] text-left bg-grayE">
@@ -5,9 +7,7 @@ const MixAndMatch = () => {
       <p className="mb-3 text-gray-500">
         Thoả sức sáng tạo với vô số sự kết hợp từ món đồ bạn thích.
       </p>
-      <button className="py-[13px] px-[12px] bg-black text-white text-[12px] font-bold">
-        SHOP NOW
-      </button>
+      <HomeButton title="SHOP NOW"></HomeButton>
       <div className="grid grid-cols-4">Image</div>
     </div>
   );
