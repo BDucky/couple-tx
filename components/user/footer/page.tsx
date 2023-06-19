121ea250731e5ee150bef3b2b938a06ddec877a1
Binhle <binh.le@quano6.com>
2023-06-20 00:06:01 +0700

    add footer social

--- components/user/footer/page.tsx
+++ components/user/footer/page.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="bg-[#111] tracking-[1px] max-w-[90rem] mx-auto my-0 p-0">
+            <div className="bg-[#111] tracking-[1px] mx-auto my-0 p-0 w-100">
                 <div className="table m-auto w-[calc(100%-40px)] max-w-[1280px] border-b-[1px] border-solid border-[#fff]">
                     <div className="flex justify-around my-[23px] mx-auto max-w-[950px] items-center">
                         <div>
@@ -17,6 +17,35 @@ const Footer = () => {
                                 <img src="https://file.hstatic.net/1000364782/file/envelope_9613e19318f14fd29c0d567d84cd31a7.svg" alt="" className="invert-[1] mr-[12px] w-[2.4375rem] h-[2.0625rem] transition-[500ms] hover:ease-in-out hover:rotate-[360deg] hover:duration-500" />
                                 <span className="text-[13px] font-normal text-[#fff]">cskh@coupletx.com</span>
                             </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="table m-auto w-[calc(100%-40px)] max-w-[1280px] border-b-[1px] border-solid border-[#fff]">
+                    <div className="table">
+                        <div className="table-cell w-[30%] p-[1.125rem]">
+                            <div className="flex gap-[20px] items-center mt-[60px]">
+                                <a href="https://www.facebook.com/CoupleTX/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                    <img src="https://theme.hstatic.net/1000184601/1000882765/14/facebook.svg?v=5116" alt="link-facebook" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
+                                </a>
+                                <a href="https://www.linkedin.com/company/coupletx/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                    <img src="https://theme.hstatic.net/1000184601/1000882765/14/linkedin.svg?v=5116" alt="link-linkedin" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
+                                </a>
+                                <a href="https://www.instagram.com/coupletx/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                    <img src="https://theme.hstatic.net/1000184601/1000882765/14/insta.svg?v=5116" alt="link-instagram" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
+                                </a>
+                                <a href="https://www.youtube.com/c/COUPLETX09/videos" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                    <img src="https://theme.hstatic.net/1000184601/1000882765/14/youtube.svg?v=5116" alt="link-youtube" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
+                                </a>
+                                <a href="https://www.tiktok.com/@coupletx?lang=vi-VN" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                    <img src="https://theme.hstatic.net/1000184601/1000882765/14/tiktok.svg?v=5116" alt="link-tiktok" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
+                                </a>
+                                <a href="https://zalo.me/2020855735863717537#" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                    <img src="https://theme.hstatic.net/1000184601/1000882765/14/zalo.png?v=5116" alt="link-zalo" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
+                                </a>
+                            </div>
+                            <div>
+                                <h3 className="border-0 p-0 mt-[30px] text-[0.8125rem] uppercase text-[#fff]">Tải app</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
