98eb1b2c92232694bbc79ed723658d020652bfac
Binhle <binh.le@quano6.com>
2023-06-20 11:08:07 +0700

    add target <a> tag

--- components/user/footer/page.tsx
+++ components/user/footer/page.tsx
@@ -24,27 +24,28 @@ const Footer = () => {
                     <div className="table">
                         <div className="table-cell w-[30%] p-[1.125rem]">
                             <div className="flex gap-[20px] items-center mt-[60px]">
-                                <a href="https://www.facebook.com/CoupleTX/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                <a href="https://www.facebook.com/CoupleTX/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]" target="_blank">
                                     <img src="https://theme.hstatic.net/1000184601/1000882765/14/facebook.svg?v=5116" alt="link-facebook" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
                                 </a>
-                                <a href="https://www.linkedin.com/company/coupletx/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                <a href="https://www.linkedin.com/company/coupletx/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]" target="_blank">
                                     <img src="https://theme.hstatic.net/1000184601/1000882765/14/linkedin.svg?v=5116" alt="link-linkedin" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
                                 </a>
-                                <a href="https://www.instagram.com/coupletx/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                <a href="https://www.instagram.com/coupletx/" className="w-[30px] h-[30px] leading-[30px] text-[#fff]" target="_blank">
                                     <img src="https://theme.hstatic.net/1000184601/1000882765/14/insta.svg?v=5116" alt="link-instagram" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
                                 </a>
-                                <a href="https://www.youtube.com/c/COUPLETX09/videos" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                <a href="https://www.youtube.com/c/COUPLETX09/videos" className="w-[30px] h-[30px] leading-[30px] text-[#fff]" target="_blank">
                                     <img src="https://theme.hstatic.net/1000184601/1000882765/14/youtube.svg?v=5116" alt="link-youtube" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
                                 </a>
-                                <a href="https://www.tiktok.com/@coupletx?lang=vi-VN" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                <a href="https://www.tiktok.com/@coupletx?lang=vi-VN" className="w-[30px] h-[30px] leading-[30px] text-[#fff]" target="_blank">
                                     <img src="https://theme.hstatic.net/1000184601/1000882765/14/tiktok.svg?v=5116" alt="link-tiktok" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
                                 </a>
-                                <a href="https://zalo.me/2020855735863717537#" className="w-[30px] h-[30px] leading-[30px] text-[#fff]">
+                                <a href="https://zalo.me/2020855735863717537#" className="w-[30px] h-[30px] leading-[30px] text-[#fff]" target="_blank">
                                     <img src="https://theme.hstatic.net/1000184601/1000882765/14/zalo.png?v=5116" alt="link-zalo" className="w-100 max-w-[30px] max-h-[30px] h-[30px]" />
                                 </a>
                             </div>
                             <div>
                                 <h3 className="border-0 p-0 mt-[30px] text-[0.8125rem] uppercase text-[#fff]">Tải app</h3>
+                                <div className="flex justify-center items-center"></div>
                             </div>
                         </div>
                     </div>
