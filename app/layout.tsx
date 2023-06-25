01e0bc63b791308a01bc2137563dd56f770b554b
Jackietruong2805 <truonghao2805@gmail.com>
2023-06-25 20:32:43 +0700

    add header and footer to rootPage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/user/footer/page";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/user/header/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header></Header>
         {children}
         <Footer></Footer>
       </body>
