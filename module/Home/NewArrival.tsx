bc017b03c490826978ed06be04090dcb99d172cb
Jackietruong2805 <truonghao2805@gmail.com>
2023-06-27 01:40:30 +0700

    Update new Arrival and header

--- module/Home/NewArrival.tsx
+++ module/Home/NewArrival.tsx
@@ -1,5 +1,3 @@
-import HomeButton from "./HomeButton";
-
 const NewArrival = () => {
   return (
     <div className="container relative overflow-hidden">
@@ -11,36 +9,31 @@ const NewArrival = () => {
           style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
         />
       </picture>
-      <div
-        className="absolute top-0 left-0 w-full h-full"
-        style={{ marginLeft: "59px" }}
-      >
-        <div className="container py-[90px] px-[30px] text-left text-3xl flex flex-col ">
-          <h2
-            className="text-6xl font-bold text-white"
-            style={{ lineHeight: "44px", marginBottom: "2px" }}
-          >
-            NEW ARRIVALS
-          </h2>
-          <p className="mb-3 text-sm text-white">
-            Những thiết kế tinh tế, mang đến xu hướng
-            <br />
-            thời trang mới
-          </p>
-          <button
-            className="btn-cus"
-            style={{
-              backgroundColor: "white",
-              color: "black",
-              width: "202px",
-              height: "42px",
-              fontSize: "15px",
-              fontWeight: "bold",
-            }}
-          >
-            Shop Now
-          </button>
-        </div>
+      <div className="absolute top-2/4 -translate-y-2/4 left-[20px] container py-[90px] px-[30px] text-left text-3xl flex flex-col gap-y-2 justify-center my-auto">
+        <h2
+          className="text-6xl font-bold text-white"
+          style={{ lineHeight: "44px", marginBottom: "2px" }}
+        >
+          NEW ARRIVALS
+        </h2>
+        <p className="mb-3 text-sm text-white">
+          Những thiết kế tinh tế, mang đến xu hướng
+          <br />
+          thời trang mới
+        </p>
+        <button
+          className="btn-cus"
+          style={{
+            backgroundColor: "white",
+            color: "black",
+            width: "202px",
+            height: "42px",
+            fontSize: "15px",
+            fontWeight: "bold",
+          }}
+        >
+          Shop Now
+        </button>
       </div>
     </div>
   );
