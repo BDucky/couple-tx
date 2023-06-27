c7da5ca51e6b3226cffb1a0bebd09653d81aa0da
MuMu0502 <phanchau0502@gmail.com>
2023-06-27 12:20:41 +0700

    blog v2

--- app/collections/post/[id]/page.tsx
+++ app/collections/post/[id]/page.tsx
@@ -12,44 +12,54 @@ const Page = ({ params }: { params: { id: string } }) => {
             postsData.push(data);
             setPosts(postsData);
         }
-        fetchData();
-    }, [params.id]);
-
-    return (
-        <LayoutWebsite>
-            <div className="w-full px-[15px]">
+        import LayoutWebsite from "@/components/layout/LayoutWebsite";
+        import React, { useEffect, useState } from "react";
+        
+        const Page = () => {
+          const [posts, setPosts] = useState([]);
+        
+          useEffect(() => {
+            async function fetchData() {
+              const res = await fetch(`http://localhost:3000/api/blog/post/findById?id=${params.id}`);
+              const data = await res.json();
+              setPosts(data);
+            }
+        
+            fetchData();
+          }, [params.id]);
+        
+          return (
+            <LayoutWebsite>
+              <div className="w-full px-[15px]">
                 <div className="md:max-w-[768px] lg:max-w-[1024px] mx-auto px-[15px]">
-                    {posts?.map((post) => (
-                        <div
-                            key={post?.id}
-                            href=""
-                            className="flex py-[30px] border-t border-gray-300 text-black no-underline"
-                        >
-                            <div className="pr-[20px] max-w-[50%]">
-                                <img
-                                    src={post?.image}
-                                    alt=""
-                                    className="w-full max-w-full transition-all duration-200 ease-in-out"
-                                />
-                            </div>
-                            <div className="md:max-w-[768px] lg:max-w-[1024px] mx-auto px-[15px]">
-                                <h1 className="text-center text-bold text-3xl">
-                                    {post?.title}
-                                </h1>
-                            </div>
-                            <article className="prose mx-auto lg:prose-xl">
-                                <div
-                                    dangerouslySetInnerHTML={{
-                                        __html: post?.body,
-                                    }}
-                                />
-                            </article>
-                        </div>
-                    ))}
+                  {posts?.map((post) => (
+                    <div key={post?.id} className="post">
+                      <div className="content">
+                        <article className="prose mx-auto lg:prose-xl">
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: post?.body,
+                            }}
+                          />
+                        </article>
+                      </div>
+                      <div className="image">
+                        <img
+                          src={post?.image}
+                          alt=""
+                          className="w-full max-w-full transition-all duration-200 ease-in-out"
+                        />
+                      </div>
+                      <div className="title">
+                        <h1 className="text-center text-bold text-3xl">{post?.title}</h1>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-            </div>
-        </LayoutWebsite>
-    );
-};
-
+              </div>
+            </LayoutWebsite>
+          );
+        };
+        
 export default Page;
+        
