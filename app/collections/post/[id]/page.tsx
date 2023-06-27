fe189aa9fdb96be3325ab2656f6d7dcc1960d3b5
Binhle <binh.le@quano6.com>
2023-06-27 13:08:57 +0700

    fix post

--- app/collections/post/[id]/page.tsx
+++ app/collections/post/[id]/page.tsx
@@ -12,57 +12,44 @@ const Page = ({ params }: { params: { id: string } }) => {
             postsData.push(data);
             setPosts(postsData);
         }
-        import LayoutWebsite from "@/components/layout/LayoutWebsite";
-        import React, { useEffect, useState } from "react";
-        
-        const Page = () => {
-          const [posts, setPosts] = useState([]);
-        
-          useEffect(() => {
-            async function fetchData() {
-              const res = await fetch(`http://localhost:3000/api/blog/post/findById?id=${params.id}`);
-              const data = await res.json();
-              setPosts(data);
-            }
-        
-            fetchData();
-          }, [params.id]);
-        
-          return (
-            <LayoutWebsite>
-              <div className="max-w-lg mx-auto p-4">
+        fetchData();
+    }, [params.id])
+
+    console.log(posts);
+
+    return (
+        <LayoutWebsite>
+            <div className="max-w-lg mx-auto p-4">
                 <div className="md:max-w-[768px] lg:max-w-[1024px] mx-auto px-[15px]">
-                  {posts?.map((post) => (
-                    <div key={post?.id} className="post">
-                      
-                        <div className="image">
-                        <img
-                          src={post?.image}
-                          alt=""
-                          className="w-full mb-4"
-                        />
-                      </div>
-                      <div className="title">
-                        <h1 className="text-center text-2xl font-bold mb-2">{post?.title}</h1>
-                      </div>
-                      <div className="content">
-                        <article className="prose mx-auto lg:prose-xl">
-                          <div
-                            dangerouslySetInnerHTML={{
-                              __html: post?.body,
-                            }}
-                          />
-                        </article>
-                      </div>
-                      
-                      
-                    </div>
-                  ))}
+                    {posts?.map((post) => (
+                        <div key={post?.id} className="post">
+                            <div className="title">
+                                <h1 className="text-center text-2xl font-bold mb-2">{post?.title}</h1>
+                            </div>
+                            <div className="image">
+                                <img
+                                    src={post?.image}
+                                    alt=""
+                                    className="w-full mb-4"
+                                />
+                            </div>
+                            <div className="content">
+                                <article className="prose mx-auto lg:prose-xl">
+                                    <div
+                                        dangerouslySetInnerHTML={{
+                                            __html: post?.body,
+                                        }}
+                                    />
+                                </article>
+                            </div>
+
+
+                        </div>
+                    ))}
                 </div>
-              </div>
-            </LayoutWebsite>
-          );
-        };
+            </div>
+        </LayoutWebsite>
+    );
+};
 
 export default Page;
-        
